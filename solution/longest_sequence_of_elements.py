d19a428ca5a8d8bf1d9e84f5ef720b04d529b4c7
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2021-04-03 18:25:02 +0300

    Code changed after feedback

--- solution/longest_sequence_of_elements.py
+++ solution/longest_sequence_of_elements.py
@@ -1,21 +1,50 @@
+import sys
 import argparse
 import numpy
+import pathlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--t1", required=True)
-parser.add_argument("--t2", required=False)
-parser.add_argument("--t3", required=False)
-parser.add_argument("--t4", required=False)
-args = parser.parse_args()
+
+def main():
+    arguments = get_arguments()
+    for t in arguments.tests:
+        run_test(t)
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tests", type=pathlib.Path, nargs="+")
+    args = parser.parse_args()
+    return args
+
+
+def run_test(test_name):
+    file = open(test_name, 'r')
+    lines = file.read().split("\n")
+    rows = int(lines[0].split()[0])
+    columns = int(lines[0].split()[1])
+    matrix = []
+    visited = numpy.full((rows, columns), False, dtype=bool)
+
+    for line in lines[1:]:
+        matrix.append(line.split(" "))
+
+    iterate_through_matrix(rows, columns, matrix, visited)
+
+
+def iterate_through_matrix(rows, columns, matrix, visited):
+    longest_sequence = 0
+    for i in range(0, rows):
+        for j in range(0, columns):
+            temp_longest_sequence = find_longest_sequence(rows, columns, i, j, 1, matrix, visited)
+            if temp_longest_sequence > longest_sequence:
+                longest_sequence = temp_longest_sequence
+    print(longest_sequence)
+
 
 current = 0
-rows = 0
-columns = 0
-matrix = []
-visited = []
 
 
-def find_longest_sequence(n, m, counter):
+def find_longest_sequence(rows, columns, n, m, counter, matrix, visited):
     global current
     current = counter
 
@@ -27,65 +56,26 @@ def find_longest_sequence(n, m, counter):
     if m + 1 < columns:
         if matrix[n][m] == matrix[n][m + 1] and not visited[n][m + 1]:
             current += 1
-            find_longest_sequence(n, m + 1, current)
+            find_longest_sequence(rows, columns, n, m + 1, current, matrix, visited)
 
     if n + 1 < rows:
         if matrix[n][m] == matrix[n + 1][m] and not visited[n + 1][m]:
             current += 1
-            find_longest_sequence(n + 1, m, current)
+            find_longest_sequence(rows, columns, n + 1, m, current, matrix, visited)
 
     if m - 1 >= 0:
         if matrix[n][m] == matrix[n][m - 1] and not visited[n][m - 1]:
             current += 1
-            find_longest_sequence(n, m - 1, current)
+            find_longest_sequence(rows, columns, n, m - 1, current, matrix, visited)
 
     if n - 1 >= 0:
         if matrix[n][m] == matrix[n - 1][m] and not visited[n - 1][m]:
             current += 1
-            find_longest_sequence(n - 1, m, current)
+            find_longest_sequence(rows, columns, n - 1, m, current, matrix, visited)
 
     return current
 
 
-def iterate_through_matrix():
-    longest_sequence = 0
-    for i in range(0, rows):
-        for j in range(0, columns):
-            temp_longest_sequence = find_longest_sequence(i, j, 1)
-            if temp_longest_sequence > longest_sequence:
-                longest_sequence = temp_longest_sequence
-    print(longest_sequence)
-
-
-def do_test(test_name):
-    global rows
-    global columns
-    global matrix
-    global visited
-
-    file = open(test_name, 'r')
-    lines = file.read().split("\n")
-    rows = int(lines[0].split()[0])
-    columns = int(lines[0].split()[1])
-    matrix = []
-    visited = numpy.full((rows, columns), False, dtype=bool)
-
-    for line in lines[1:]:
-        matrix.append(line.split(" "))
-
-    iterate_through_matrix()
-
-
-def main():
-    if args.t1:
-        do_test(args.t1)
-    if args.t2:
-        do_test(args.t2)
-    if args.t3:
-        do_test(args.t3)
-    if args.t4:
-        do_test(args.t4)
-
-
 if __name__ == "__main__":
+    sys.setrecursionlimit(10**6)
     main()
